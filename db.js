0f59136cbe0388979e7b8fbb1643b6a253241753
ga-mi-keke <niyaahyao@gmail.com>
2025-03-01 20:17:06 +0900

    Backup before destructive changes

--- db.js
+++ db.js
@@ -2,13 +2,12 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  host: process.env.DB_HOST,  // Railwayの環境変数に合わせる
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
-
 connection.connect(err => {
   if (err) {
     console.error('❌ Database connection failed:', err);
